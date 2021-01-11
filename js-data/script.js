e3295f95ab3342bd4ee6d113561035208c0ad186
Begimai-A <68447915+Begimai-A@users.noreply.github.com>
2021-01-11 13:54:21 +0300

    get and set dates

---
+++ js-data/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// const now=new Date(2023, 6, 1, 20);
+// console.log(now);
+
+// const now=new Date();
+// console.log(now.getFullYear());
+
+// const now=new Date();
+// console.log(now.getMonth());
+
+//const now=new Date();
+//console.log(now.getDate());// getHours, getMinutes, getSeconds
+
+//const now=new Date();
+//console.log(now.getDay());// Позволяет получить день недели. Нумерация начинается с воскресенья[0]
+
+//const now=new Date();
+//console.log(now.getUTCHours());
+
+const now=new Date();
+console.log(now.setHours(15));// можно установить часы
+console.log(now);
